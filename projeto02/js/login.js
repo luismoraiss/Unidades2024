923518933e34b210dafd4eb630689b4bd9626d21
Luis Morais <138785109+luismoraiss@users.noreply.github.com>
2024-02-21 12:40:30 -0300

    etet

---
+++ projeto02/js/login.js
@@ -0,0 +1,18 @@
+import data from "../data.json" assert { "type": "json" };
+
+const btn = document.querySelector("#login");
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const user = document.getElementById("user").value;
+  const password = document.getElementById("password").value;
+
+  const login = data.find(
+    (obj) => obj.usuario === user && obj.senha === password
+  );
+
+  if (login) {
+    window.location = "../index.html";
+  } else {
+    alert("etetete");
+  }
+});
---
+++ projeto02/js/person.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',() =>{
+
+const urlParam = new URLSearchParams(window.location.search)
+const paramIndex =  urlParam.get('index')
+})
+
+function personagem(){
+
+    personagemImg.innerHTML 
+    
+    
+}
